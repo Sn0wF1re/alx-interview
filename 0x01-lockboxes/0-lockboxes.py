968645552af12a9847a66dc6afe113c3df92bae3
Sn0wF1re <mokayaclinton34@gmail.com>
2023-04-25 12:01:10 +0300

    Attempt the lockboxes challenge

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1
+and each box may contain keys to the other boxes
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened
+    Return True if possible, False otherwise
+    """
+    if not boxes or not isinstance(boxes, list):
+        return False
+
+    keys = set((0,))
+
+    for i in range(len(boxes)):
+        for k in boxes[i]:
+            keys.add(k)
+
+    return len(keys) == len(boxes)
