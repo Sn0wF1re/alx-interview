bcb60cf573ee8e767913bb2073fb352b2c623e18
Sn0wF1re <mokayaclinton34@gmail.com>
2023-05-11 11:10:23 +0300

    Write a script that reads stdin line by line and computes metrics

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Write a script that reads stdin line by line and computes metric
+"""
+import sys
+
+
+total_size = 0
+counter = 0
+code_count = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+try:
+    for line in sys.stdin:
+        data_list = line.split()
+        if len(data_list) > 3:
+            total_size += int(data_list[-1])
+            status_code = data_list[-2]
+            if status_code in code_count.keys():
+                code_count[status_code] += 1
+            counter += 1
+
+        if counter == 10:
+            print(f"File size: {total_size}")
+            for k, v in sorted(code_count.items()):
+                if v > 0:
+                    print(f"{k}: {v}")
+            counter = 0
+
+except Exception:
+    pass
+
+finally:
+    print(f"File size: {total_size}")
+    for k, v in sorted(code_count.items()):
+        if v > 0:
+            print(f"{k}: {v}")
