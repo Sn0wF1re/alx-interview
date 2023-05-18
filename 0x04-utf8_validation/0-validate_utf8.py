87384aac2466d49281590739addf4bebaf3c14c2
Sn0wF1re <mokayaclinton34@gmail.com>
2023-05-18 17:27:53 +0300

    Change approach to see if all checks pass

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,21 +8,24 @@ def validUTF8(data):
     """
     determines if a given data set represents a valid UTF-8 encoding
     """
-    byte_count = 0
+    subsequent_bytes = 0
 
     for num in data:
-        if byte_count > 0:
-            if num >> 6 == 2:
-                byte_count -= 1
+        binary = bin(num)[2:].zfill(8)
+        if subsequent_bytes == 0:
+            if binary.startswith('0'):
+                continue
+            elif binary.startswith('110'):
+                subsequent_bytes = 1
+            elif binary.startswith('1110'):
+                subsequent_bytes = 2
+            elif binary.startswith('11110'):
+                subsequent_bytes = 3
             else:
                 return False
 
         else:
-            mask = 1 << 7
-            while mask & num:
-                byte_count += 1
-                mask >>= 1
-            if byte_count > 4 or byte_count == 1:
+            if not binary.startswith('10'):
                 return False
-
-    return byte_count == 0
+            subsequent_bytes -= 1
+    return subsequent_bytes == 0
