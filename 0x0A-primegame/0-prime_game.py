ffc58f58a87e66f59ea2373a0d789187b122c9e9
Sn0wF1re <mokayaclinton34@gmail.com>
2023-06-27 11:57:54 +0300

    Change approach

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,51 +1,23 @@
-#!/usr/bin/python3
-"""
-Prime game
-"""
-
-
-def is_prime(n):
-    """
-    Check if number is a prime number
-    """
-    primes = []
-    prime_flags = [True] * (n + 1)
-    prime_flags[0] = prime_flags[1] = False
-
-    p = 2
-    while p * p <= n:
-        if prime_flags[p]:
-            primes.append(p)
-            for i in range(p * p, n + 1, p):
-                prime_flags[i] = False
-        p += 1
-    for num in range(p, n + 1):
-        if prime_flags[num] and num not in primes:
-            primes.append(num)
-    return primes
-
-
 def isWinner(x, nums):
-    """
-    Find winner of prime game after x rounds
-    """
-    maria_wins = 0
-    ben_wins = 0
+    wins = {"Maria": 0, "Ben": 0}
 
     for n in nums:
-        if n < 2:
-            ben_wins += 1
+        if n == 1:
+            wins["Ben"] += 1
         else:
-            primes = is_prime(n)
-
-            if len(primes) % 2 == 0:
-                ben_wins += 1
+            prime_count = sum(1 for num in range(2, n + 1)
+                              if all(num % i != 0
+                              for i in range(2, int(num ** 0.5) + 1)))
+            if prime_count % 2 == 0:
+                wins["Ben"] += 1
             else:
-                maria_wins += 1
+                wins["Maria"] += 1
 
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    max_wins = max(wins.values())
+
+    if wins["Maria"] == wins["Ben"]:
         return None
+    elif wins["Maria"] == max_wins:
+        return "Maria"
+    else:
+        return "Ben"
